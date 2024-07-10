3dfe636dbcb828e47b17df41453f2e731a7a5f30
outisa <savolainen.outi@gmail.com>
2024-07-10 21:09:23 +0300

    Read data from files

--- utils/handle_data.py
+++ utils/handle_data.py
@@ -1,15 +1,9 @@
-"""_summary_
-
-Returns:
-    _type_: _description_
-"""
 import torch
-import pandas as pd
 import numpy as np
-from utils.helper_functions import do_fft
 import glob
-import matplotlib.pyplot as plt
-
+import os
+import math
+from utils.helper_functions import do_fft
 
 def get_data_files(files_to_read):
     print(files_to_read)
@@ -19,5 +13,28 @@ def get_data_files(files_to_read):
     print("Number of files:", len(data_files))
     return data_files
  
-def read_data_from_dat(files_to_read, year, window, n_packets):
-    pass
+def read_data_from_dat(data_set, files_to_read, window, step_size, BW, sample_interval=20, len_seq=4):
+    data_files = get_data_files(files_to_read)
+    for numerator, file_name in enumerate(data_files):
+        print(f'File {numerator+1}/{len(data_files)} - ',end=' ')
+        name = file_name.replace('.DAT', '').split('/')[-1]
+        name = name[-1]
+        bin_type = np.int16
+
+        file_size = os.path.getsize(file_name)
+        total_ms = int(file_size / BW / 4) * 1000
+
+        after_fft = []
+        for j in range(0, int(total_ms), sample_interval):
+            # here the bin file of integer is read, and then it will be converted to Complex
+            offset_ = int(math.floor(j * BW * 4/1000))
+            if offset_ + step_size * 2 * len_seq > file_size:
+                break
+            raw_signal = np.fromfile(file_name, dtype=bin_type, offset=offset_, count=(step_size * 2 * len_seq))
+            raw_signal = raw_signal.astype(np.float32).view(np.complex64)
+
+            for i in range(0, len_seq):
+                after_fft.append(do_fft(raw_signal[i * step_size: (i +1) * step_size], window, BW))
+        print(f'Number of samples after fft: {len(after_fft)}')
+        after_fft = np.array(after_fft)
+        data_set[name] = torch.tensor(after_fft)

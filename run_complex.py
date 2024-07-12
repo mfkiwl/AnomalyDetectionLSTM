7e68f03ab390228bc41838e080781c7602e0bb3e
outisa <savolainen.outi@gmail.com>
2024-07-12 15:00:00 +0300

    Update run_complex.py

--- run_complex.py
+++ run_complex.py
@@ -29,11 +29,15 @@ FEATURES = 1024
 HIDDEN_DIM = 450
 LEN_SEQ = 4
 WINDOW = FEATURES
+STEP_SIZE = 1
+BANDWIDTH = int(25e6)
+sample_interval = 200
 model_path = f'best_cv_model_{datetime.datetime}.pt'
 
 # %%
 # reate sequences for training data
-data = read_data_from_dat('your_path_dat/*.DAT', WINDOW)
+data = {}
+read_data_from_dat(data, 'your_path_dat/*.DAT', WINDOW, STEP_SIZE, BANDWIDTH, sample_interval, LEN_SEQ)
 
 #%%
 train_data, list_of_endings_tr, train_class = [], [] ,[]
@@ -47,9 +51,7 @@ start_total = time.time()
 train_set, val_set, train_y, val_y = train_test_split(train_data, train_class, test_size=0.2, random_state=42)
 criterion = custom_mse_criterion
 (
-    trained_model,
-    train_losses_final,
-    val_losses_final,
+    trained_model
 ) = train_model(
     MODEL,
     train_set,
@@ -79,7 +81,7 @@ end_sample = 0
 to_test_sequences(test_data, testData, LEN_SEQ, list_of_endings)
 #%%
 start = time.time()
-loss_vals, predicted_vals = predict(MODEL, val_set, LEN_SEQ, FEATURES, DEVICE, criterion)
+loss_vals, predicted_vals = predict(MODEL, val_set, LEN_SEQ, FEATURES, DEVICE, criterion, dtype=np.complex128)
 end = time.time()
 epoch_time(start, end)
 #%%
